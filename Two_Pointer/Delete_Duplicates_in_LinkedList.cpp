cb8caa37ceed06c0b3b19896831b150e29d40720
Aparna-256 <rosary.aparna@gmail.com>
2026-01-23 21:59:06 +0530

    Solution for Dlete Duplicates In LinkedList

---
+++ Two_Pointer/Delete_Duplicates_in_LinkedList.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+
+        ListNode* temp = head;
+
+        while(temp != NULL && temp->next != NULL) {
+
+            if(temp->val == temp->next->val) {
+                ListNode* duplicate = temp->next;
+                temp->next = temp->next->next;
+                delete duplicate;
+            }
+            else {
+                temp = temp->next;
+            }
+        }
+        return head;
+    }
+};
